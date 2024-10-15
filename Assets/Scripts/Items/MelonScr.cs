cc7033df15ab0e2a2a998a31ce7d5a02f43ca369
CowBroth <535945@escg.ac.uk>
2024-10-15 16:07:27 +0100

    melon

--- Assets/Scripts/Items/MelonScr.cs
+++ Assets/Scripts/Items/MelonScr.cs
@@ -6,27 +6,33 @@ public class MelonScr : MonoBehaviour
 {
     GameObject a;
     MelonCountScr b;
-    Animation c;
-    IEnumerator d;
+    Animator c;
     BoxCollider2D e;
     void Start()
     {
         a = GameObject.Find("MelonCounter");
         b = a.GetComponent<MelonCountScr>();
-        c = GetComponent<Animation>();
+        c = GetComponent<Animator>();
         e = GetComponent<BoxCollider2D>();
-
-        c.Play("m_melon");
+        c.GetBool("Col");
     }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            e.gameObject.SetActive(false);
             b.melonCount++;
-            c.Play("m_col");
-            new WaitForSeconds(0.717f);
-            Destroy(gameObject);
+            e.enabled = false;
+            c.SetBool("Col", true);
+            StartCoroutine(F());
         }
+    }
+    void D()
+    {
+        Destroy(gameObject);
+    }
+    IEnumerator F()
+    {
+        yield return new WaitForSeconds(0.717f);
+        D();
     }
 }
